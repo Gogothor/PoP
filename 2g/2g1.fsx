79c92a3f27a58026eab030910e29f2fa4fd4e969
Alexander Bang <>
2022-09-23 10:03:58 +0200

    added documentation for other functions

--- 2g/2g1.fsx
+++ 2g/2g1.fsx
@@ -31,12 +31,30 @@ let mul ((a1, b1): vec) (c: float) : vec = a1 * c, b1 * c
 let rot ((a, b): vec) (r: float) : vec =
     (a * cos (r) - b * sin (r), a * sin (r) + b * cos (r))
 
+/// <summary>
+///  Given a vector, og two float values, convert to integer
+/// </summary>
+/// <param name="vec">Vector</param>
+/// <returns>New Vector in integers</returns>
 let toInt ((a, b): vec) : int * int = int (a), int (b)
 
+/// <summary>
+///  Given a point and a vector, draw a line from the point to the vector
+/// </summary>
+/// <param name="c">Canvas</param>
+/// <param name="color">Color og the line</param>
+/// <param name="v">Vector</param>
+/// <param name="p">Point</param>
+/// <returns>Unit</returns>
 let setVector (c: canvas) (color: color) (v: vec) (p: vec) : unit =
     setLine c color (toInt p) (toInt (add v p))
 
-
+/// <summary>
+///  Draw function for the show function to display
+/// </summary>
+/// <param name="w">Width of window</param>
+/// <param name="h">Height of window</param>
+/// <returns>Canvas</returns>
 let draw w h =
     let c = create w h
     let centerx = float (w / 2)
