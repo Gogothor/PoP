0bb8f20794ccabd4af588a428ce2ea0c7a315bb6
Gogothor <Tgregersen998@gmail.com>
2022-10-06 14:54:15 +0200

    Update 4g0c.fsx
    Added printfn statements

--- 4g/src/4g0c.fsx
+++ 4g/src/4g0c.fsx
@@ -5,6 +5,7 @@ let dist (p1: pos) (p2: pos) : int =
     let (x1, y1) = p1
     let (x2, y2) = p2
     (pown (x2 - x1) 2 + pown (y2 - y1) 2)
+printfn "%A" (dist(1,3)(2,3))
 
 let candidates (src: pos) (tg: pos) : pos list =
     let (x, y) = src
@@ -14,9 +15,8 @@ let candidates (src: pos) (tg: pos) : pos list =
           (x - 1, y)
           (x, y + 1)
           (x, y - 1) ]
-
     List.filter (fun (p: pos) -> dist src tg >= dist p tg) cand
-
+printfn "%A" (candidates(3,3)(2,2))
 
 let rec routes (src: pos) (tg: pos) : pos list list =
     if src = tg then
@@ -26,3 +26,4 @@ let rec routes (src: pos) (tg: pos) : pos list list =
         |> List.map (fun (elem: pos) -> routes elem tg)
         |> List.collect id
         |> List.map (fun (elem: pos list) -> src :: elem)
+printfn "%A" (routes(3,3)(1,1))
